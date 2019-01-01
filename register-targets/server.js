27a6e7fcdbe562b7d49581bb99c9d37ce0b891c1
Mathew Moon <mmoon@athena-iot.com>
2019-01-01 13:10:41 -0600

    Add finalized response

--- register-targets/server.js
+++ register-targets/server.js
@@ -4,57 +4,45 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var AWS = require('aws-sdk');
 var elb = new AWS.ELBv2();
+
 app.enable('trust proxy');
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-var port = (process.env.PORT || process.env.VCAP_APP_PORT || 80);
+
 
 
 app.post('/log-info', function(req, res) {
 	console.log(JSON.stringify(req.body));
-    res.json({ok: true});
+  res.json({ok: true});
 });
 
 app.post('/register', function(req, res){
+	var port = (process.env.PORT || process.env.VCAP_APP_PORT || 80);
 	var targetGroupArn = JSON.stringify(req.body.object.metadata.annotations.targetGroupArn);
 	var targetIp = JSON.stringify(req.body.object.status.podIP);
 	var targetPort = JSON.stringify(req.body.object.metadata.annotations.targetPort);
 
-	console.log("Load Balancer Target Group: " + targetGroupArn);
-	console.log("Target IP: " + targetIp + " Port: " + targetPort);
-
 	var params = {
-	  TargetGroupArn: targetGroupArn,
-	  Targets: [
-	    {
-	      Id: targetIp,
-	      AvailabilityZone: 'all',
-	      Port: targetPort
-	    },
-	  ]
-	};
+		TargetGroupArn: targetGroupArn,
+		Targets: [
+			{
+				Id: targetIp,
+				AvailabilityZone: 'all',
+				Port: targetPort
+			},
+		]
+	}
 
 	elb.registerTargets(params, function(err, data) {
 	  if(err){
 			console.log(err, err.stack);
-			res.status(500).json({ error: 'failed to register target' });
+			res.status(500).json({ error: 'Failed to register target.' });
 		}else{
 			console.log(data);
-			res.status(200).json({response: JSON.stringify(data)});
+			res.status(200).json({response: "Registered target: " + targetIp + ":" + targetPort + "to " + targetGroupArn});
 		}
 	});
 });
-
-var deregister = function(params){
-	elb.deregisterTargets(params, function(err, data) {
-		if(err){
-			console.log(err, err.stack);
-			return false;
-		}else{
-			return data;
-		}
-	});
-}
 
 app.post('/deregister', function(req, res){
 	var targetGroupArn = JSON.stringify(req.body.object.metadata.annotations.targetGroupArn);
@@ -70,24 +58,17 @@ app.post('/deregister', function(req, res){
 	      Port: targetPort
 	    },
 	  ]
-	};
-
-	for(retries = 0, retries < 4; retries++; ){
-		if(retries == 3){
-			res.status(500).json({ error: 'Failed to deregister target. Giving up.' });
-			break;
-		}
-
-		var result = deregister(params);
-		if(result){
-			console.log("Registered target: " + targetIp + ":" + targetPort + "to " + targetGroupArn);
-			console.log(result);
-			res.status(200).json({response: "Registered target: " + targetIp + ":" + targetPort + "to " + targetGroupArn});
-			break;
-		}else{
-			setTimeout(deregister, 5000, params)
-		}
 	}
+
+	elb.deregisterTargets(params, function(err, data) {
+		if(err){
+			console.log(err, err.stack);
+			res.status(500).json({response: 'Failed to deregister target.', finalized: false });
+		}else{
+			console.log(data)
+			res.status(200).json({response: "Degistered target: " + targetIp + ":" + targetPort + "to " + targetGroupArn, finalized: true});
+		}
+	});
 });
 
 var server = app.listen(port, function() {
